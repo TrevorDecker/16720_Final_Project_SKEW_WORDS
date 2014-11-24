a6ac0e8ac47436fc9388422648e6fc62f8f5bb14
sarahttan <sarah.t.tan@gmail.com>
2014-11-24 15:07:05 -0500

    Updated findLetters with boundingbox orientation and improved image thresholding
    findLetters calls the function thresholdImage_v2, so call findLetters on
    any RGB image to get back the bounding boxes, orientation, area, and
    center of mass.

--- code/findLetters.m
+++ code/findLetters.m
@@ -1,32 +1,37 @@
 function [ B ] = findLetters( imagen )
 %findLetters returns the top left corner of the letter, the bounding box of each letter, and the letter it was most likely associated with.
 %  Inputs: Im - image
-%   Outputs: B - nx1 structure where the B.BoundingBox represents bounding box (x,y,w,h),
-%               B.Area represents the area of each bounding box
-%               B.Centroid represents the centroid of each bounding box
+%   Outputs: B - nx1 structure where
+%               B.BoundingBox - bounding box (x,y,w,h) --> to crop image at a given bounding box (image, B(i).BoundingBox)
+%               just call imcrop(
+%               B.Area - the area of each bounding box
+%               B.Centroid - the center of mass of the region within the bounding box (the centroid where things are located)
+%               B.Orientation - orientation to make letter upright
 figure; imshow(imagen);
 
 %% Threshold Image into Binary Image
-imagen = thresholdImage(imagen);
+imagen = thresholdImage_v2(imagen);
 %% Label connected components
 [L Ne]=bwlabel(imagen);
 %% Measure properties of image regions
 propied=regionprops(L,'BoundingBox','Area','Orientation','Centroid');
 hold on
 %% Plot Bounding Box
+% Comment this out to turn off plotting overlay on images
 for n=1:size(propied,1)
     rectangle('Position',propied(n).BoundingBox,'EdgeColor','g','LineWidth',2)
 end
 hold off
 pause (1)
 %% Objects extraction
+% To see each object extraction uncomment this.
 %figure
-for n=1:Ne
-    [r,c] = find(L==n);
-    n1=imagen(min(r):max(r),min(c):max(c));
-    %figure;imshow(~n1);
-    %pause(0.5)
-end
+% for n=1:Ne
+%     [r,c] = find(L==n);
+%     n1=imagen(min(r):max(r),min(c):max(c));
+%     %figure;imshow(~n1);
+%     %pause(0.5)
+% end
 
 B =propied;
 end
